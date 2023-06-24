7571593cdce97bfcea5e46ac60608b292de46327
LuiyiBarrantes <luiyibarrantes@gmail.com>
2023-06-24 03:41:23 -0300

    vista favoritas terminado, trabajo practico DONE!

--- frontend/js/favoritas.js
+++ frontend/js/favoritas.js
@@ -1,38 +1,58 @@
-window.onload = () => {
+const $ = (id) => document.getElementById(id);
+const qs = (id) => document.querySelector(id);
+window.onload = async () => {
   const app = document.getElementById("root");
   const container = document.createElement("div");
   container.setAttribute("class", "container");
   app.appendChild(container);
-
+  const favorites = localStorage.getItem("favorites") ? JSON.parse(localStorage.getItem("favorites")) : [];
+  console.log(favorites);
   // Aqui debemos agregar nuestro fetch
 
 
+  favorites.forEach(async id => {
+    try {
+      let response = await fetch('http://localhost:3031/api/movies/'+id)
+    let movie = await response.json()
 
-  /** Codigo que debemos usar para mostrar los datos en el frontend
-    let data = peliculas.data;
+    
+  /** /* Codigo que debemos usar para mostrar los datos en el frontend */
+    let data = movie.data;
 
-    data.forEach((movie) => {
+    
       const card = document.createElement("div");
       card.setAttribute("class", "card");
 
       const h1 = document.createElement("h1");
-      h1.textContent = movie.title;
+      h1.textContent = data.title;
 
       const p = document.createElement("p");
-      p.textContent = `Rating: ${movie.rating}`;
+      p.textContent = `Rating: ${data.rating}`;
 
       const duracion = document.createElement("p");
-      duracion.textContent = `Duración: ${movie.length}`;
+      duracion.textContent = `Duración: ${data.length}`;
 
       container.appendChild(card);
       card.appendChild(h1);
       card.appendChild(p);
-      if (movie.genre !== null) {
+      if (data.genre !== null) {
         const genero = document.createElement("p");
-        genero.textContent = `Genero: ${movie.genre.name}`;
+        genero.textContent = `Genero: ${data.genre.name}`;
         card.appendChild(genero);
       }
       card.appendChild(duracion);
-    });
-  */
+    ;
+  } catch (error) {
+      console.log(error);
+    }
+  });
+  //si un usuario quiere ir a la vista de favoritos sin tener favoritos, le imprime en la vista el siguiente mensaje
+  if (favorites.length===0) {
+    const alert = document.createElement('h2')
+    alert.innerText='NO HAY PELICULAS AGREGADAS A FAVORITOS';    
+    qs('.container').appendChild(alert)
+  }
+    
+    
+
 };
